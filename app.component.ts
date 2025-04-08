ea470959a9f55f18fff9c1e50e4b473a657affaf
xandarcaGe <162327120+xandarcaGe@users.noreply.github.com>
2025-04-08 07:27:47 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,23 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { RouterOutlet } from '@angular/router';
+
+
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'front-end';
+}
